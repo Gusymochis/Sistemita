8c512528937491e57f3b6786c3c329873aba894d
Corona-Ayala, Manuel <Manuel_Corona-Ayala@intuit.com>
2015-11-01 21:40:35 -0800

    inital beta release

--- flaskr.py
+++ flaskr.py
@@ -25,17 +25,48 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_houses():
-    cur = g.db.execute('select nombre, vendedor, construccion, terreno, precio, balance, status from casa order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('pages/show_houses.html', entries=entries)
+    cur = g.db.execute('select id, nombre, direccion, vendedor, construccion, terreno, valor, balance, status from casa order by id asc')
+    casas = [dict(id=row[0], nombre=row[1], direccion=row[2], vendedor=row[3], construccion=row[4], terreno=row[5], valor=row[6],
+             balance=row[7], status=row[8]) for row in cur.fetchall()]
+    return render_template('pages/show_houses.html', casas=casas)
+
+@app.route('/getSeller')
+def show_sellers():
+    cur = g.db.execute('select nombre, apellidos, telefono, email, balance from vendedor order by id desc')
+    vendedores = [dict(nombre=row[0], apellidos=row[1], telefono=row[2], email=row[3], balance=row[4]) for row in cur.fetchall()]
+    return render_template('pages/show_sellers.html', vendedores=vendedores)
 
 @app.route('/addHouse', methods=['GET', 'POST'])
 def add_house():
     error = None
     if not session.get('logged_in'):
         abort(401)
-    flash('not yet implemented')
+    if request.method == 'POST':
+        g.db.execute('insert into casa (nombre, direccion, vendedor, construccion, terreno, valor, balance, status) values (?, ?, ?, ?, ?, ?, ? ,?)', 
+            [request.form['nombre'],request.form['direccion'],request.form['vendedor'],request.form['construccion'],request.form['terreno'],
+             request.form['valor'],request.form['balance'],request.form['status']])
+        g.db.commit()
+        flash('La casa fue agregada satisfactoriamente')
+    else:
+        cur = g.db.execute('select id, nombre, apellidos from vendedor order by id desc')
+        vendedores = [dict(id=row[0], nombre=row[1], apellidos=row[2]) for row in cur.fetchall()]
+        return render_template('pages/add_house.html', vendedores=vendedores)
     return render_template('pages/add_house.html', error=error)
+
+@app.route('/addSeller', methods=['GET', 'POST'])
+def add_seller():
+    error = None
+    if not session.get('logged_in'):
+        abort(401)
+    print request.method
+    if request.method == 'POST':
+        g.db.execute('insert into vendedor (nombre, apellidos, telefono, email, balance) values (?, ?, ? ,? ,?)',
+                     [request.form['nombre'],request.form['apellidos'],request.form['telefono'],request.form['email'],request.form['balance']])        
+        g.db.commit()
+        flash("Vendedor agregado exitosamente!")
+        return redirect(url_for('add_seller'))
+    else:
+        return render_template('pages/add_seller.html', error=error)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- schema.sql
+++ schema.sql
@@ -4,16 +4,20 @@ drop table if exists vendedor;
 create table vendedor (
 	id integer primary key autoincrement,
 	nombre text not null,
+	apellidos text not null,
+	telefono number not null,
+	email text not null,
 	balance float
 );
 
 create table casa (
 	id integer primary key autoincrement,
 	nombre text not null,
+	direccion text not null,
 	vendedor integer,
 	construccion float,
 	terreno float,
-	precio float,
+	valor float,
 	balance float,
 	status integer not null,
 	FOREIGN KEY(vendedor) REFERENCES vendedor(id)
